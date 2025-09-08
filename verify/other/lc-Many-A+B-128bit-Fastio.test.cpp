3852c4552b41e6f804d1616f25d7eda21ddd3c3e
yohaku0v0 <yohakuhrsw@gmail.com>
2025-09-09 02:55:23 +0900

    Create lc-Many-A+B-128bit-Fastio.test.cpp

---
+++ verify/other/lc-Many-A+B-128bit-Fastio.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb_128bit"
+
+#include "cpstl/other/Template.hpp"
+
+int main() {
+	int T;
+	cpstd::input(T);
+	__int128 A, B;
+	while (T--) {
+		cpstd::input(A, B);
+		cpstd::println(A + B);
+	}
+}
